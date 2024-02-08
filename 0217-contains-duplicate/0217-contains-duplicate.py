355b89a9fecc36f36a67fa7309094bc31c88b4bd
Elias Firisa <125973507+EliasSf73@users.noreply.github.com>
2024-02-09 03:42:45 +0900

    Time: 422 ms (85.26%), Space: 27.6 MB (60.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        Set=list(set(nums))
+        return len(nums)!=len(Set)
+        # print(Set)
+        # print(nums)
+        
