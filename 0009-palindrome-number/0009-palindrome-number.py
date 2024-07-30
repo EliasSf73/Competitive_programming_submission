2eb145792bccf3db944ce350c03b2e9eb02cd06c
Elias Firisa <125973507+EliasSf73@users.noreply.github.com>
2024-07-31 07:31:21 +0900

    Time: 60 ms (8.16%), Space: 11.6 MB (60.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,5 +4,23 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        return str(x)==str(x)[::-1]
+        original=x
+       
+        # if x==0:
+        #     return True
+        if x<0 :
+            return False
+        li=[]
+        while x>0:
+            rem=x%10
+            x=x//10
+            li.append(rem)
+        
+        # print(li)
+        new=0
+        
+        for i in range(len(li)):
+            new+=li[i]*(10**(len(li)-i-1))
+        # print(new)
+        return new==original 
         
